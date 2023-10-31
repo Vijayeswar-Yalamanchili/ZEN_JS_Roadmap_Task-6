230669f35f2ca782cf9b650ef49a7064c10c3396
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2023-10-31 11:53:28 +0530

    Q1 & Q3 completed

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Q1  : 
+// Q1  : https://github.com/rvsp/typescript-oops/blob/master/Practice/Movie.md
 // Ans : 
 
-// Q2  : 
+class Movie {
+    constructor(title,studio,rating = "PG"){
+        this.title  = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+}
+
+const movie1 = new Movie("Casino Royale","Eon Productions","PG13")
+console.log(movie1.title);
+console.log(movie1.studio);
+console.log(movie1.rating);
+const movie2 = new Movie("LEO","7 studios","PG17");
+console.log(movie2.title);
+console.log(movie2.studio);
+console.log(movie2.rating);
+
+// Q2  : https://github.com/rvsp/typescript-oops/blob/master/Practice/class-circle.md
 // Ans : 
+
+
 
 // Q3  : Write a “person” class to hold all the details.
 // Ans : 
@@ -23,3 +42,5 @@ console.log(person1.gender);
 console.log(person1.salary);
 
 // Q4  : write a class to calculate the uber price.
+// Ans :
+
